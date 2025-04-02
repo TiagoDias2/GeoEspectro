5bad3523cf0327445ee4da6729a748efdbf95d7f
Tiago Miguel Gonçalves Dias <aluno24721@ipt.pt>
2025-04-02 21:28:26 +0100

    Base

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GeoEspectro.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
