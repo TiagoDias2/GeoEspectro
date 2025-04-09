539e02d9b6624d9da66ab6e08b18ff7a08b6e012
Tiago Miguel Gonçalves Dias <aluno24721@ipt.pt>
2025-04-09 10:47:06 +0100

    Acrescento da class detalhes

---
+++ Models/Detalhes.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GeoEspectro.Models
+{
+    public class Detalhes
+    {
+        /// <summary>
+        /// Detalhe Multimedia Princapal
+        /// </summary>
+        public bool principal { get; set; }
+
+        /// <summary>
+        /// ForeignKey para referenciar o utilizador ao recurso
+        /// </summary>
+        [ForeignKey(nameof(Recurso))]
+        public int RecursoFK { get; set; }
+
+        /// <summary>
+        /// ForeignKey para referenciar o utilizador ao recurso
+        /// </summary>
+        public Recursos Recurso { get; set; }
+
+        /// <summary>
+        /// ForeignKey para o artigo que o utilizador gostou
+        /// </summary>
+        [ForeignKey(nameof(Artigo))]
+        public int ArtigoFK { get; set; }
+
+        /// <summary>
+        /// ForeignKey para o artigo que o utilizador gostou
+        /// </summary>
+        public Artigos Artigo { get; set; }
+
+    }
+}
