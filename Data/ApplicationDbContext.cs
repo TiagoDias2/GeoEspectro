2ebe970cffd060ce4c218ed33b8949e5a59a0b0f
Joot111 <joaoamoroso04@gmail.com>
2025-04-13 16:00:26 +0100

    Controller Recursos e View Recursos

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using GeoEspectro.Models;
 
 namespace GeoEspectro.Data;
 
@@ -9,4 +10,6 @@ public class ApplicationDbContext : IdentityDbContext
         : base(options)
     {
     }
+
+public DbSet<GeoEspectro.Models.Recursos> Recursos { get; set; } = default!;
 }
