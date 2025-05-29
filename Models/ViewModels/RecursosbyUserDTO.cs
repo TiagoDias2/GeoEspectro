25f8623181d7af19584775191f287fcbff4fac28
Joot111 <joaoamoroso04@gmail.com>
2025-05-29 16:45:47 +0100

    Início da utilização da API

---
+++ Models/ViewModels/RecursosbyUserDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace GeoEspectro.Models.ViewModels
+{
+    /// <summary>
+    /// Dados de um recurso multimédia para serem usados na API
+    /// </summary>
+    public class RecursosbyUserDTO : RecursosDTO
+    {
+        /// <summary>
+        /// Nome do Dono de um Recurso
+        /// </summary>
+        public string DonoRecurso { get; set; } = "";
+    }
+}
